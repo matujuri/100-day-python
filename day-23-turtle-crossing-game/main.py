4949e1a3ba430e76a714e2d09943f18e4acc90f2
matujuri <matujuri@yahoo.com>
2025-06-19 16:16:00 +0900

    新しいカーレースゲームの実装を追加しました。CarManager、Player、Scoreboardクラスを作成し、ゲームの進行管理を行うメインファイルを実装しました。プレイヤーの操作や車の生成、衝突検出、レベルアップ機能を含んでいます。

---
+++ day-23-turtle-crossing-game/main.py
@@ -0,0 +1,36 @@
+from turtle import Screen
+import time
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.title("Turtle Crossing Game")
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.move, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car_manager.create_car()
+    car_manager.move_cars()
+    
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+    
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+screen.exitonclick()
