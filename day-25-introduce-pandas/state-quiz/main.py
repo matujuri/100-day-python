03adf4bed40662b0c2d37761939cfd45029b2c16
matujuri <matujuri@yahoo.com>
2025-06-23 17:14:33 +0900

    新しい州クイズゲームのためのCSVファイルとPythonスクリプトを追加しました。CSVファイルにはアメリカの州名とその座標が含まれており、スクリプトはユーザーが州名を入力して位置を表示する機能を実装しています。また、州の位置を示す画像ファイルも追加しました。

---
+++ day-25-introduce-pandas/state-quiz/main.py
@@ -0,0 +1,34 @@
+from turtle import Turtle, Screen
+import pandas as pd
+
+screen = Screen()
+screen.title("U.S. States Game")
+screen.setup(width=725, height=491)
+image = "blank_states_img.gif"
+screen.addshape(image)
+t = Turtle()
+t.shape(image)
+
+data = pd.read_csv("50_states.csv")
+writer = Turtle()
+writer.penup()
+writer.hideturtle()
+
+guessed_states = []
+while len(guessed_states) < 50:
+    input_state = screen.textinput(title=f"{len(guessed_states)}/50 Guess the State", prompt="What's another state's name?")
+
+    if input_state.title() in data.state.values:
+        writer.goto(data[data.state == input_state.title()].x.item(), data[data.state == input_state.title()].y.item())
+        writer.write(input_state.title())
+        guessed_states.append(input_state.title())
+
+screen.exitonclick()
+
+
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+
+# screen.onscreenclick(get_mouse_click_coor)
+
+# screen.mainloop()
