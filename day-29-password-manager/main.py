471c6379e440b18cc8ef1df1cda45f1f2e687dd7
matujuri <matujuri@yahoo.com>
2025-06-25 16:39:37 +0900

    パスワードマネージャーにパスワード保存機能を追加し、入力フィールドの検証を行うようにしました。また、データをCSVファイルに保存する処理を実装しました。

--- day-29-password-manager/main.py
+++ day-29-password-manager/main.py
@@ -1,8 +1,27 @@
-from tkinter import Tk, Label, Button, Entry, Canvas, PhotoImage
+from tkinter import Tk, Label, Button, Entry, Canvas, PhotoImage, END, messagebox
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+        return
+    
+    is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?")
+    
+    if is_ok:
+        # Clear the entries
+        website_entry.delete(0, END)
+        password_entry.delete(0, END) 
+
+        # Save the data to a CSV file
+        with open("data.csv", "a") as data_file:
+            data_file.write(f"{website},{email},{password}\n")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -26,9 +45,11 @@ password_label.grid(column=0, row=3)
 
 website_entry = Entry(width=35)
 website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
 
 email_entry = Entry(width=35)
 email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "test@test.com")
 
 password_entry = Entry()
 password_entry.grid(column=1, row=3, sticky="ew")
@@ -36,7 +57,7 @@ password_entry.grid(column=1, row=3, sticky="ew")
 generate_password_button = Button(text="Generate Password", width=12)
 generate_password_button.grid(column=1, row=3, sticky="e", padx=(0,0))
 
-add_button = Button(text="Add", width=32)
+add_button = Button(text="Add", width=32, command=save_password)
 add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()   
