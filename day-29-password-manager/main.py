76d0a932f8ebcfb6567a0b45b882577dab8b8433
matujuri <matujuri@yahoo.com>
2025-06-25 15:25:48 +0900

    パスワードマネージャーのUIを改善し、ウェブサイト、メール/ユーザー名、パスワードの入力フィールドを追加しました。また、パスワード生成ボタンと追加ボタンを配置しました。

--- day-29-password-manager/main.py
+++ day-29-password-manager/main.py
@@ -8,10 +8,35 @@ from tkinter import Tk, Label, Button, Entry, Canvas, PhotoImage
 window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
+window.grid_columnconfigure(1, weight=1)
 
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
+canvas.grid(column=0, row=0, columnspan=3)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2)
+
+email_entry = Entry(width=35)
+email_entry.grid(column=1, row=2, columnspan=2)
+
+password_entry = Entry()
+password_entry.grid(column=1, row=3, sticky="ew")
+
+generate_password_button = Button(text="Generate Password", width=12)
+generate_password_button.grid(column=1, row=3, sticky="e", padx=(0,0))
+
+add_button = Button(text="Add", width=32)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()   
