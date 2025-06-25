cdd6913e6dd35874fe4df3ceed8bf03edacf7c66
matujuri <matujuri@yahoo.com>
2025-06-25 17:00:30 +0900

    パスワード生成機能を追加し、パスワードの生成ロジックを実装しました。また、入力フィールドの検証を強化し、警告ラベルを追加してユーザーにフィードバックを提供するようにしました。

--- day-29-password-manager/main.py
+++ day-29-password-manager/main.py
@@ -1,27 +1,38 @@
-from tkinter import Tk, Label, Button, Entry, Canvas, PhotoImage, END, messagebox
+from tkinter import Tk, Label, Button, Entry, Canvas, PhotoImage, END
+import random
+import string
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+symbols = "!#$%&()*+"
+
+def generate_password():
+    password_list = random.choices(string.ascii_letters, k=random.randint(8, 10))
+    password_list += random.choices(string.digits, k=random.randint(2, 4))
+    password_list += random.choices(symbols, k=random.randint(2, 4))
+    random.shuffle(password_list)
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    password_entry.clipboard_clear()
+    password_entry.clipboard_append(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
-    
+
+    warning_label.config(text="")
     if len(website) == 0 or len(password) == 0:
-        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+        warning_label.config(text="Please don't leave any fields empty!", fg="red")
         return
     
-    is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?")
-    
-    if is_ok:
-        # Clear the entries
-        website_entry.delete(0, END)
-        password_entry.delete(0, END) 
+    # Clear the entries
+    website_entry.delete(0, END)
+    password_entry.delete(0, END) 
 
-        # Save the data to a CSV file
-        with open("data.csv", "a") as data_file:
-            data_file.write(f"{website},{email},{password}\n")
+    # Save the data to a CSV file
+    with open("data.csv", "a") as data_file:
+        data_file.write(f"{website},{email},{password}\n")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -54,10 +65,13 @@ email_entry.insert(0, "test@test.com")
 password_entry = Entry()
 password_entry.grid(column=1, row=3, sticky="ew")
 
-generate_password_button = Button(text="Generate Password", width=12)
+generate_password_button = Button(text="Generate Password", width=12, command=generate_password)
 generate_password_button.grid(column=1, row=3, sticky="e", padx=(0,0))
 
 add_button = Button(text="Add", width=32, command=save_password)
 add_button.grid(column=1, row=4, columnspan=2)
+
+warning_label = Label(text="", fg="red")
+warning_label.grid(column=1, row=5, columnspan=2)
 
 window.mainloop()   
