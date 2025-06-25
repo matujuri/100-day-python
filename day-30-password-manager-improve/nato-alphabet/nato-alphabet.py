bcfc9f6c5edf1294d76bbb19e298474c8877b1fd
matujuri <matujuri@yahoo.com>
2025-06-25 22:36:56 +0900

    ナトアルファベットのスクリプトにエラーハンドリングを追加し、ユーザーがアルファベット以外の文字を入力した場合に警告メッセージを表示するようにしました。

--- day-30-password-manager-improve/nato-alphabet/nato-alphabet.py
+++ day-30-password-manager-improve/nato-alphabet/nato-alphabet.py
@@ -3,5 +3,9 @@ dataframe = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter:row.code for (index, row) in dataframe.iterrows()}
 
 input_word = input("Enter a word: ").upper()
-output_list = [nato_dict[letter] for letter in input_word]
-print(output_list)
+try:
+    output_list = [nato_dict[letter] for letter in input_word]
+except KeyError:
+    print("Sorry, only letters in the alphabet please.")
+else:
+    print(output_list)
