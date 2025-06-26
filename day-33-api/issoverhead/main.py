42c5fe6dd105607dab7789d249efc43d12b88764
matujuri <matujuri@yahoo.com>
2025-06-26 23:22:39 +0900

    新しいスクリプトを追加し、国際宇宙ステーション（ISS）が上空にあるかどうかを確認し、夜間にメール通知を送信する機能を実装しました。位置情報と日没・日の出の時間を考慮して、定期的にチェックを行います。

---
+++ day-33-api/issoverhead/main.py
@@ -0,0 +1,56 @@
+import requests
+from datetime import datetime
+import time
+import smtplib
+
+MY_LAT = 35.443707
+MY_LNG = 139.638031
+MY_EMAIL = "b38860114@gmail.com"
+MY_PASSWORD = "app password"
+
+def is_iss_overhead():
+    response = requests.get("http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()["iss_position"]
+    latitude = data["latitude"]
+    longitude = data["longitude"]
+    
+    if MY_LAT - 5 <= float(latitude) <= MY_LAT + 5 and MY_LNG - 5 <= float(longitude) <= MY_LNG + 5:
+        return True
+    else:
+        return False
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LNG,
+        "formatted": 0
+    }
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+
+    data = response.json()
+    sunrise = data["results"]["sunrise"]
+    sunset = data["results"]["sunset"]
+    sunrise_hour = int(datetime.fromisoformat(sunrise).hour)
+    sunset_hour = int(datetime.fromisoformat(sunset).hour)
+
+    time_now = datetime.now().hour
+    # it is night if the time is between sunset and sunrise
+    if time_now >= sunset_hour or time_now <= sunrise_hour:
+        return True
+    else:
+        return False
+
+
+while True:
+    if is_iss_overhead() and is_night():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs="xiaobaka59@gmail.com",
+                msg="Subject:Look up!\n\nThe ISS is above you in the sky."
+            )   
+    time.sleep(60)
