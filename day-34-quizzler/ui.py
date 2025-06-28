362f26d45308aacc56fcef784cfe8ce5e140d58c
matujuri <matujuri@yahoo.com>
2025-06-29 08:50:27 +0900

    クイズアプリのロジックを改善し、質問を表示するメソッドを更新しました。また、ユーザーインターフェースにボタンの機能を追加し、正誤をチェックする処理を実装しました。

--- day-34-quizzler/ui.py
+++ day-34-quizzler/ui.py
@@ -1,12 +1,14 @@
 from tkinter import Canvas, Tk, PhotoImage, Button, Label
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz: QuizBrain):
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+        self.quiz = quiz
 
         self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
         self.score_label.grid(row=0, column=1)
@@ -15,18 +17,25 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(
             150,
             125,
-            text="Some Question Text",
+            text=self.quiz.next_question(),
             fill=THEME_COLOR,
-            font=("Arial", 20, "italic")
+            font=("Arial", 20, "italic"),
+            width=280
         )
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
         
         true_image = PhotoImage(file="day-34-quizzler/images/true.png")
-        self.true_button = Button(image=true_image, highlightthickness=0, borderwidth=0)
+        self.true_button = Button(image=true_image, highlightthickness=0, borderwidth=0, command=self.true_pressed)
         self.true_button.grid(row=2, column=0)
         
         false_image = PhotoImage(file="day-34-quizzler/images/false.png")
-        self.false_button = Button(image=false_image, highlightthickness=0, borderwidth=0)
+        self.false_button = Button(image=false_image, highlightthickness=0, borderwidth=0, command=self.false_pressed)
         self.false_button.grid(row=2, column=1)
         
         self.window.mainloop()
+        
+    def true_pressed(self):
+       self.quiz.check_answer("True")
+        
+    def false_pressed(self):
+        self.quiz.check_answer("False")
