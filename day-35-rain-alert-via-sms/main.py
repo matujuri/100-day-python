b74c017cc74c64d9785098642c14230db9afb9c8
matujuri <matujuri@yahoo.com>
2025-06-29 11:20:13 +0900

    新しい天気予報アプリのメインスクリプトを追加し、OpenWeatherMap APIを使用して降雨予測を取得する機能を実装しました。また、.gitignoreに.envファイルを追加しました。

---
+++ day-35-rain-alert-via-sms/main.py
@@ -0,0 +1,37 @@
+import requests
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = os.getenv("OWM_API_KEY")
+
+parameters = {
+    "lat": 35.467560,
+    "lon": 139.625586,
+    "appid": api_key,
+    "cnt": 8
+}
+
+response = requests.get(OWM_ENDPOINT, params=parameters)
+response.raise_for_status()
+
+data = response.json()
+
+weather_data = data["list"]
+# 6時から21時までのデータを取得
+for weather in weather_data[1:7]:
+    if weather["weather"][0]["id"] < 700:
+        print("Bring an umbrella")
+        break
+else:
+    print("No umbrella needed")
+
+
+
+
+
+
+
+
