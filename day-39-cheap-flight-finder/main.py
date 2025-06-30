0ee6732c4f108818a0863e68d968676d24393cca
matujuri <matujuri@yahoo.com>
2025-06-30 18:06:25 +0900

    最安フライト取得メソッドをリファクタリングし、出発地と目的地のフライト情報をそれぞれ更新する処理を改善しました。また、環境変数から通知先アドレスを取得する機能を追加しました。

--- day-39-cheap-flight-finder/main.py
+++ day-39-cheap-flight-finder/main.py
@@ -5,7 +5,7 @@ from data_manager import DataManager
 
 origin = "TYO"
 
-def get_lowest_price_and_update_data(origin: str, destination: str, is_departure: bool):
+def get_cheapest_flight(origin: str, destination: str):
     flight_search = FlightSearch(origin=origin, destination=destination)
     cheapest_flight = flight_search.get_cheapest_flight_in_a_year()
     if cheapest_flight == {}:
@@ -16,14 +16,15 @@ def get_lowest_price_and_update_data(origin: str, destination: str, is_departure
         destination=cheapest_flight["destination"],
         value=cheapest_flight["value"]
     )
-    print(flight_data.__str__())
-    if is_departure:
-        data_manager.update_price(destination_data.index(destination) + 2, flight_data.value, flight_data.depart_date, True)
-    else:
-        data_manager.update_price(destination_data.index(origin) + 2, flight_data.value, flight_data.depart_date, False)
+    return flight_data
 
 data_manager = DataManager()
 destination_data = data_manager.get_destination_data()
 for destination in destination_data:
-    get_lowest_price_and_update_data(origin, destination, True)
-    get_lowest_price_and_update_data(destination, origin, False)
+    departure_flight_data = get_cheapest_flight(origin, destination)
+    if departure_flight_data is not None:
+        data_manager.update_price(destination_data.index(destination) + 2, departure_flight_data.value, departure_flight_data.depart_date, True)
+    return_flight_data = get_cheapest_flight(destination, origin)
+    if return_flight_data is not None:
+        data_manager.update_price(destination_data.index(destination) + 2, return_flight_data.value, return_flight_data.depart_date, False)
+    
