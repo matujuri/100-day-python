eca9869cf8cbda7f4d57404a71509308e1485d05
matujuri <matujuri@yahoo.com>
2025-06-30 16:40:50 +0900

    DataManagerクラスを追加し、Google Sheets APIとの連携機能を実装しました。FlightDataクラスを構築し、フライトデータの管理を行うメソッドを追加しました。メインスクリプトでデスティネーションデータを取得し、最安フライト情報を表示する処理を追加しました。

--- day-39-cheap-flight-finder/data_manager.py
+++ day-39-cheap-flight-finder/data_manager.py
@@ -1,3 +1,30 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    SHEETY_ENDPOINT = "https://api.sheety.co/5e9221bf09d3ba652c755231464449ec/cheapFlightFinder/sheet1"
+    
+    def __init__(self):
+        self.headers = {
+            "Authorization": f"Bearer {os.getenv('SHEETY_TOKEN')}"
+        }
+    
+    
+    def get_destination_data(self)->list[str]:
+        with open("day-39-cheap-flight-finder/destination_data.txt", "r") as file:
+            destination_data = file.read().split(",")
+            
+        if destination_data == ['']:
+            response = requests.get(self.SHEETY_ENDPOINT, headers=self.headers)
+            response.raise_for_status()
+            destination_data = [row["iataCode"] for row in response.json()["sheet1"]]
+            with open("day-39-cheap-flight-finder/destination_data.txt", "w") as file:
+                file.write(",".join(destination_data))
+        
+        return destination_data
+    
+    
--- day-39-cheap-flight-finder/main.py
+++ day-39-cheap-flight-finder/main.py
@@ -1,9 +1,20 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from flight_search import FlightSearch
+from flight_data import FlightData
+from data_manager import DataManager
 
 origin = "TYO"
-destination = "SHA"
+destination = ""
 
-flight_search = FlightSearch(origin=origin, destination=destination)
-cheapest_flight = flight_search.get_cheapest_flight_in_six_months()
-print(cheapest_flight)
+data_manager = DataManager()
+destination_data = data_manager.get_destination_data()
+for destination in destination_data:
+    flight_search = FlightSearch(origin=origin, destination=destination)
+    cheapest_flight = flight_search.get_cheapest_flight_in_six_months()
+    flight_data = FlightData(
+        depart_date=cheapest_flight["depart_date"],
+        origin=cheapest_flight["origin"],
+        destination=cheapest_flight["destination"],
+        value=cheapest_flight["value"]
+    )
+    print(flight_data.__str__())
