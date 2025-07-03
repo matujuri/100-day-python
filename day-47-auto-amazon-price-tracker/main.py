35496f460df3e48c77d5db70a3eaba918a805005
matujuri <matujuri@yahoo.com>
2025-07-03 16:38:46 +0900

    Amazonの価格トラッキング機能を更新し、URLと価格閾値を変更しました。価格取得のロジックを修正し、通知メッセージに表示する価格を整形しました。

--- day-47-auto-amazon-price-tracker/main.py
+++ day-47-auto-amazon-price-tracker/main.py
@@ -8,8 +8,8 @@ load_dotenv()
 
 USER_AGENT = os.getenv("USER_AGENT")
 ACCEPT_LANGUAGE = os.getenv("ACCEPT_LANGUAGE")
-URL = "https://appbrewery.github.io/instant_pot/"
-PRICE_THRESHOLD = 100
+URL = "https://www.amazon.co.jp/dp/B09S3B1F8H?ref=ppx_yo2ov_dt_b_fed_asin_title&th=1"
+PRICE_THRESHOLD = 3900
 
 HEADERS = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", 
@@ -30,18 +30,16 @@ HEADERS = {
 response = requests.get(URL, headers=HEADERS)
 
 soup = BeautifulSoup(response.text, "html.parser")
-
 price_whole = soup.find(name="span", class_="a-price-whole")
-price_fraction = soup.find(name="span", class_="a-price-fraction")
-print(price_whole.getText() + price_fraction.getText())
+price=price_whole.getText().replace(",", "")
 
 notification_manager = NotificationManager()
 # send email if price is less than 100
-if float(price_whole.getText() + price_fraction.getText()) < PRICE_THRESHOLD:
+if int(price) < PRICE_THRESHOLD:
     notification_manager.send_email(
         to_addrs="xiaobaka59@gmail.com",
         subject="Amazon Price Alert!",
-        message=f"The price of the product is now {price_whole.getText() + price_fraction.getText()}"
+        message=f"The price of the product is now {price}"
     )
 
 
