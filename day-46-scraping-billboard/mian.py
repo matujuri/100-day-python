717e852ccdd219b0e9b6537872bb4343b9703ec1
matujuri <matujuri@yahoo.com>
2025-07-03 15:20:47 +0900

    Billboard Hot 100チャートから曲名を取得し、Spotifyプレイリストを作成して曲を追加する機能を実装しました。また、Spotify APIとの連携を強化し、プレイリストをブラウザで開く機能を追加しました。

--- day-46-scraping-billboard/mian.py
+++ day-46-scraping-billboard/mian.py
@@ -1,11 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
-from spotify_manager import get_song_link, add_song_to_playlist
+from spotify_manager import get_track_uri, add_track_to_playlist, open_playlist_in_browser, create_playlist
 
-input_year = input("Which year do you want to travel to? Type the date in this format: YYYY-MM-DD: ")
+# Firstly, authenticate in spotify_manager.py, then save token to .env file
 
-def get_song_names_from_billboard(input_year) -> list[str]:
-    URL = f"https://www.billboard.com/charts/hot-100/{input_year}"
+memory_day = input("Which year do you want to travel to? Type the date in this format: YYYY-MM-DD: ")
+
+PLAYLIST_ID = create_playlist(memory_day)
+
+def get_track_names_from_billboard(memory_day) -> list[str]:
+    URL = f"https://www.billboard.com/charts/hot-100/{memory_day}"
 
     response = requests.get(URL, headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"})
     website_html = response.text
@@ -15,7 +19,15 @@ def get_song_names_from_billboard(input_year) -> list[str]:
 
     return [song.getText().strip() for song in song_name_h3]
 
-song_names = get_song_names_from_billboard(input_year)
-song_links = [get_song_link(song) for song in song_names if get_song_link(song)]
-for song_link in song_links:
-    add_song_to_playlist(song_link)
+# get song names from billboard
+track_names = get_track_names_from_billboard(memory_day)
+
+# get track uris from spotify
+track_uris = [get_track_uri(track) for track in track_names if get_track_uri(track)]
+
+# add track uris to playlist
+for track_uri in track_uris:
+    add_track_to_playlist(track_uri, PLAYLIST_ID)
+
+# open playlist in browser after adding tracks
+open_playlist_in_browser(PLAYLIST_ID)
--- day-46-scraping-billboard/spotify_manager.py
+++ day-46-scraping-billboard/spotify_manager.py
@@ -3,18 +3,18 @@ from spotipy.oauth2 import SpotifyOAuth
 import os
 from dotenv import load_dotenv
 import requests
+import webbrowser
+
 load_dotenv()
 
 CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 TOKEN = os.getenv("SPOTIFY_TOKEN")
 USER_ID = os.getenv("SPOTIFY_USER_ID")
-PLAYLIST_NAME = "2011-01-01 Billboard 100"
-PLAYLIST_ID = os.getenv("SPOTIFY_PLAYLIST_ID")
 
-def get_song_link(song_name) -> str | None:
+def get_track_uri(track_name) -> str | None:
     response = requests.get(
-        url=f'https://api.spotify.com/v1/search?q={song_name}&type=track&limit=1',
+        url=f'https://api.spotify.com/v1/search?q={track_name}&type=track&limit=1',
         headers={
             'Authorization': f'Bearer {TOKEN}'
         }
@@ -24,18 +24,37 @@ def get_song_link(song_name) -> str | None:
     else:
         return None
     
-def add_song_to_playlist(song_link: str):
+def add_track_to_playlist(track_uri: str, playlist_id: str):
     response = requests.post(
-        url=f'https://api.spotify.com/v1/playlists/{PLAYLIST_ID}/tracks',
+        url=f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks',
         headers={
             'Authorization': f'Bearer {TOKEN}'
         },
         json={
-            'uris': [song_link]
+            'uris': [track_uri]
         }
     )
     print(response.json())
+    
+def create_playlist(memory_day: str) -> str:
+    response = requests.post(
+        url=f'https://api.spotify.com/v1/users/{USER_ID}/playlists',
+        headers={
+            'Authorization': f'Bearer {TOKEN}'
+        },
+        json={
+            'name': f'{memory_day} Billboard 100',
+            'description': f'100 music from {memory_day}',
+            'public': False
+        }
+    )
+    return response.json()["id"]
 
+def open_playlist_in_browser(playlist_id: str):
+    webbrowser.open(f"https://open.spotify.com/playlist/{playlist_id}")
+    
+
+# First, authenticate in spotify_manager.py, then save token to .env file
 def authenticate():
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
         client_id=CLIENT_ID,
@@ -50,17 +69,5 @@ def authenticate():
     for item in results['items']:
         track = item['track']
         print(track['name'], '-', track['artists'][0]['name'])
-    
-def create_playlist():
-    response = requests.post(
-        url=f'https://api.spotify.com/v1/users/{USER_ID}/playlists',
-        headers={
-            'Authorization': f'Bearer {TOKEN}'
-        },
-        json={
-            'name': PLAYLIST_NAME,
-            'description': '100 music from date in past',
-            'public': False
-        }
-    )
-    print(response.json()["id"])
+        
+# authenticate()
