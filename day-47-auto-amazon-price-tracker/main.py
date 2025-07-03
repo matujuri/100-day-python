e429cf37314043ed9bea90bf2c796abab5aac104
matujuri <matujuri@yahoo.com>
2025-07-03 16:32:19 +0900

    HEADERSに新しいHTTPヘッダーを追加し、リクエストの詳細を強化しました。

--- day-47-auto-amazon-price-tracker/main.py
+++ day-47-auto-amazon-price-tracker/main.py
@@ -12,8 +12,19 @@ URL = "https://appbrewery.github.io/instant_pot/"
 PRICE_THRESHOLD = 100
 
 HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", 
+    "Accept-Encoding": "gzip, deflate, br, zstd", 
     "Accept-Language": ACCEPT_LANGUAGE, 
-    "User-Agent": USER_AGENT
+    "Priority": "u=0, i", 
+    "Sec-Ch-Ua": "\"Not)A;Brand\";v=\"8\", \"Chromium\";v=\"138\", \"Google Chrome\";v=\"138\"", 
+    "Sec-Ch-Ua-Mobile": "?0", 
+    "Sec-Ch-Ua-Platform": "\"macOS\"", 
+    "Sec-Fetch-Dest": "document", 
+    "Sec-Fetch-Mode": "navigate", 
+    "Sec-Fetch-Site": "cross-site", 
+    "Sec-Fetch-User": "?1", 
+    "Upgrade-Insecure-Requests": "1", 
+    "User-Agent": USER_AGENT, 
 }
 
 response = requests.get(URL, headers=HEADERS)
