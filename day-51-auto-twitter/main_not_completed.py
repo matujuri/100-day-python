655f3076d93a3c70480cb4b14e01690ebac6b7b7
matujuri <matujuri@yahoo.com>
2025-07-06 11:46:16 +0900

    新しいスクリプトmain_not_completed.pyを追加し、Seleniumを使用してTwitterに自動ログインする機能を実装しました。環境変数からユーザー名とパスワードを取得し、ログインプロセスを自動化します。 セキュリティ厳しいため、ログインできない

---
+++ day-51-auto-twitter/main_not_completed.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://x.com/i/flow/login")
+
+email_input = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//input[@autocomplete='username']"))
+)
+email_input.send_keys(os.getenv("TWITTER_USERNAME"))
+print("email inputted")
+
+next_button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), '次へ')]"))
+)
+next_button.click()
+print("next button clicked")
+
+password_input = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//input[@autocomplete='current-password']"))
+)
+password_input.send_keys(os.getenv("TWITTER_PASSWORD"))
+print("password inputted")
+
+login_button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'ログイン')]"))
+)
+login_button.click()
+print("login button clicked")
+
+
+
+
+
+    
+    
+    
