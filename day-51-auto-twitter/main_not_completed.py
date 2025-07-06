0b2e199a3f132e774d7fed21d259b5a4e6afbc00
matujuri <matujuri@yahoo.com>
2025-07-06 12:03:09 +0900

    Twitterへの自動ログイン機能を改善し、ネット速度を測定する機能を追加しました。ログインプロセスを関数化し、速度テストの結果を表示する処理を実装しました。

--- day-51-auto-twitter/main_not_completed.py
+++ day-51-auto-twitter/main_not_completed.py
@@ -11,36 +11,50 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 
-driver.get("https://x.com/i/flow/login")
 
-email_input = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.XPATH, "//input[@autocomplete='username']"))
-)
-email_input.send_keys(os.getenv("TWITTER_USERNAME"))
-print("email inputted")
+def get_net_speed():
+    driver.get("https://www.speedtest.net/ja")
 
-next_button = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), '次へ')]"))
-)
-next_button.click()
-print("next button clicked")
-
-password_input = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.XPATH, "//input[@autocomplete='current-password']"))
-)
-password_input.send_keys(os.getenv("TWITTER_PASSWORD"))
-print("password inputted")
-
-login_button = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'ログイン')]"))
-)
-login_button.click()
-print("login button clicked")
-
-
-
-
-
+    go_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'Go')]"))
+    )
+    go_button.click()
     
+    sleep(60)
     
+    download_speed = driver.find_element(By.CLASS_NAME, "download-speed")
+    print(download_speed.text)
     
+    upload_speed = driver.find_element(By.CLASS_NAME, "upload-speed")
+    print(upload_speed.text)
+
+def login_to_twitter(): 
+    driver.get("https://x.com/i/flow/login")
+
+    email_input = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//input[@autocomplete='username']"))
+    )
+    email_input.send_keys(os.getenv("TWITTER_USERNAME"))
+    print("email inputted")
+
+    next_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), '次へ')]"))
+    )
+    next_button.click()
+    print("next button clicked")
+
+    password_input = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//input[@autocomplete='current-password']"))
+    )
+    password_input.send_keys(os.getenv("TWITTER_PASSWORD"))
+    print("password inputted")
+
+    login_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'ログイン')]"))
+    )
+    login_button.click()
+    print("login button clicked")
+
+
+
+get_net_speed()
