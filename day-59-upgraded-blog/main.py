917863b6201ede9c43d331deeb3e2a6cdcabad64
matujuri <matujuri@yahoo.com>
2025-07-09 12:10:04 +0900

    Flaskアプリケーションにブログ投稿を表示する機能を追加し、投稿データを外部APIから取得するための新しいクラス（Posts）を作成しました。また、index.htmlを更新して投稿リストを表示し、個別の投稿を表示するための新しいルートを追加しました。header.htmlとfooter.htmlを使用して、テンプレートの一貫性を保ちました。

--- day-59-upgraded-blog/main.py
+++ day-59-upgraded-blog/main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from post import Posts
 
 app = Flask(__name__)
+posts = Posts()
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=posts.all_posts)
 
 @app.route("/about")
 def about():
@@ -13,6 +15,10 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/post/<int:id>")
+def post(id):
+    return render_template("post.html", post=posts.get_post(id))
 
 if __name__ == "__main__":
     app.run(debug=True)
