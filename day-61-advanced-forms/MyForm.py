273ac6dde27152eab84c51318fcf9ab63f4ce627
matujuri <matujuri@yahoo.com>
2025-07-10 14:40:24 +0900

    MyFormクラスのバリデーションを強化し、EmailFieldに無効なメールアドレスのエラーメッセージを追加しました。また、login.htmlにおいて、エラーメッセージの表示形式をリストに変更し、視認性を向上させました。

--- day-61-advanced-forms/MyForm.py
+++ day-61-advanced-forms/MyForm.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length
 
 class MyForm(FlaskForm):
-    email = EmailField(label='Email', validators=[DataRequired()])
-    password = PasswordField(label='Password', validators=[DataRequired()])
+    email = EmailField(label='Email', validators=[DataRequired(),Email(message="Invalid email address")])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
     submit = SubmitField(label='Log In')
