24f5453e07c138f9d32150f95d8c4bdba7979675
matujuri <matujuri@yahoo.com>
2025-07-10 14:13:58 +0900

    MyFormクラスのフィールドをEmailFieldとPasswordFieldに変更し、ログインフォームに送信ボタンを追加しました。また、login.htmlのHTML構造を整理し、フォームのアクションを更新しました。

--- day-61-advanced-forms/MyForm.py
+++ day-61-advanced-forms/MyForm.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import EmailField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 class MyForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
-    password = StringField('Password', validators=[DataRequired()])
+    email = EmailField(label='Email', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label='Log In')
