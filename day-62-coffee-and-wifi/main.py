4a53edfd77b07bd0d5b782e104dd10b937e8c6a1
matujuri <matujuri@yahoo.com>
2025-07-11 15:22:12 +0900

    カフェフォームにリクエストを追加し、コーヒー、Wi-Fi、電源の選択肢を1から5に修正しました。また、CSVファイルへの書き込み時に改行を追加し、テンプレートのスタイルを微調整しました。

--- day-62-coffee-and-wifi/main.py
+++ day-62-coffee-and-wifi/main.py
@@ -1,23 +1,10 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, TimeField
 from wtforms.validators import DataRequired, URL
 import csv
 from datetime import time
-
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -26,38 +13,33 @@ Bootstrap5(app)
 
 class CafeForm(FlaskForm):
     cafe = StringField('Cafe name', validators=[DataRequired()])
-    location = StringField('Location', default="https://goo.gl/maps/ALR8iBiNN6tVfuAA8", validators=[DataRequired(), URL()])
+    location = StringField('Location', validators=[DataRequired(), URL()])
     open = TimeField('Open', default=time(8, 0, 0), validators=[DataRequired()])
     close = TimeField('Close', default=time(17, 0, 0), validators=[DataRequired()])
-    coffee = SelectField('Coffee', validators=[DataRequired()], choices=[(str(i), i * '☕️') for i in range(6)])
-    wifi = SelectField('Wifi', validators=[DataRequired()], choices=[(str(i), i * '💪') for i in range(6)])
-    power = SelectField('Power', validators=[DataRequired()], choices=[(str(i), i * '🔌') for i in range(6)])
+    coffee = SelectField('Coffee', validators=[DataRequired()], choices=[(i * '☕️', i) for i in range(1, 6)]) # type: ignore
+    wifi = SelectField('Wifi', validators=[DataRequired()], choices=[(i * '💪', i) for i in range(1, 6)]) # type: ignore
+    power = SelectField('Power', validators=[DataRequired()], choices=[(i * '🔌', i) for i in range(1, 6)]) # type: ignore
     submit = SubmitField('Submit')
-
-# Exercise:
-# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
-# make coffee/wifi/power a select element with choice of 0 to 5.
-#e.g. You could use emojis ☕️/💪/✘/🔌
-# make all fields required except submit
-# use a validator to check that the URL field has a URL entered.
-# ---------------------------------------------------------------------------
-
 
 # all Flask routes below
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
 @app.route('/add', methods=['GET', 'POST'])
 def add_cafe():
     form = CafeForm()
-    if form.validate_on_submit():
+    if request.method == 'POST' and form.validate_on_submit():
         with open('day-62-coffee-and-wifi/cafe-data.csv', mode='a', encoding='utf-8') as csv_file:
-            csv_file.write(f"\n{form.cafe.data}, {form.location.data}, {form.open.data.strftime('%H:%M') if form.open.data else ''}, {form.close.data.strftime('%H:%M') if form.close.data else ''}, {form.coffee.data}, {form.wifi.data}, {form.power.data}")
+            csv_file.write(f"\n{form.cafe.data}, \
+                           {form.location.data}, \
+                           {form.open.data.strftime('%H:%M') if form.open.data else ''}, \
+                           {form.close.data.strftime('%H:%M') if form.close.data else ''}, \
+                           {form.coffee.data}, \
+                           {form.wifi.data}, \
+                           {form.power.data}")
         return redirect(url_for('cafes'))
     return render_template('add.html', form=form)
-
 
 @app.route('/cafes')
 def cafes():
