5bc2885f145be10d64d75a1d842ea997a1ebbf40
matujuri <matujuri@yahoo.com>
2025-07-11 22:12:00 +0900

    映画データベース機能を追加し、映画モデルを定義しました。また、ホームページのテンプレートを更新し、映画情報を表示するようにしました。SQLiteデータベースを設定し、テーブルを作成する処理を追加しました。

--- day-64-top-movies/main.py
+++ day-64-top-movies/main.py
@@ -8,32 +8,35 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies.db"
 Bootstrap5(app)
 
 # CREATE DB
+class Base(DeclarativeBase):
+    pass
 
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 
 # CREATE TABLE
-
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String, nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=False)
+    review: Mapped[str] = mapped_column(String, nullable=False)
+    img_url: Mapped[str] = mapped_column(String, nullable=False)
+    
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", movies=db.session.execute(db.select(Movie).order_by(Movie.rating)).scalars())
 
 
 if __name__ == '__main__':
