59cdaa96295b91616e84bdc56fdce0446d07f5b3
matujuri <matujuri@yahoo.com>
2025-07-11 16:27:15 +0900

    書籍モデルを追加し、データベースURIを新しい書籍コレクション用に変更しました。また、サンプルデータの追加をコメントアウトしました。

--- day-63-virtual-bookshelf/sqlite-pactice/main.py
+++ day-63-virtual-bookshelf/sqlite-pactice/main.py
@@ -5,17 +5,27 @@ from sqlalchemy import Integer, String, Float
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Book(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(unique=True)
-    email: Mapped[str]
+    title: Mapped[str] = mapped_column(unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(nullable=False)
+    rating: Mapped[float] = mapped_column(nullable=False)
 
 # create the app
 app = Flask(__name__)
 # configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
 # initialize the app with the extension
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
+    
+    # book = Book(
+    #     id=1,
+    #     title="Harry Potter",
+    #     author="J. K. Rowling",
+    #     rating=9.3
+    # )
+    # db.session.add(book)
+    # db.session.commit()
