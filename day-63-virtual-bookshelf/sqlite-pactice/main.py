fc4cb136e8d16e27a6632fe30c4bae8a71a0cfc3
matujuri <matujuri@yahoo.com>
2025-07-11 16:17:21 +0900

    Flaskアプリケーションにおいて、SQLAlchemyを使用してデータベース接続を設定し、Userモデルを追加しました。また、アプリケーションコンテキスト内でデータベースのテーブルを作成する機能を実装しました。

--- day-63-virtual-bookshelf/sqlite-pactice/main.py
+++ day-63-virtual-bookshelf/sqlite-pactice/main.py
@@ -1,9 +1,21 @@
-import sqlite3
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
-db = sqlite3.connect("day-63-virtual-bookshelf/sqlite-pactice/books-collection.db")
-cursor = db.cursor()
+db = SQLAlchemy()
 
-# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str]
 
-cursor.execute("INSERT INTO books VALUES (1, 'Harry Potter', 'J. K. Rowling', '9.3')")
-db.commit()
+# create the app
+app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+# initialize the app with the extension
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
