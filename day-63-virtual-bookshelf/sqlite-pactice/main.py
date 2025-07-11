ac5c7841b3d0395f52f0cc4f6ebf5204992841a4
matujuri <matujuri@yahoo.com>
2025-07-11 16:06:37 +0900

    書籍テーブルの作成をコメントアウトし、サンプルデータとして「Harry Potter」を追加しました。

--- day-63-virtual-bookshelf/sqlite-pactice/main.py
+++ day-63-virtual-bookshelf/sqlite-pactice/main.py
@@ -3,5 +3,7 @@ import sqlite3
 db = sqlite3.connect("day-63-virtual-bookshelf/sqlite-pactice/books-collection.db")
 cursor = db.cursor()
 
-cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
 
+cursor.execute("INSERT INTO books VALUES (1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+db.commit()
