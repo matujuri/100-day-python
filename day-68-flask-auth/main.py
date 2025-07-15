dae94e295f3a47e1782e1b1f085385ae47edd628
matujuri <matujuri@yahoo.com>
2025-07-15 15:45:35 +0900

    ユーザー登録機能を強化し、POSTリクエストによる新規ユーザーの作成を実装しました。また、秘密のページでユーザー名を表示するようにテンプレートを更新しました。

--- day-68-flask-auth/main.py
+++ day-68-flask-auth/main.py
@@ -5,24 +5,26 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 import os
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 # CREATE DATABASE
-
-
 class Base(DeclarativeBase):
     pass
-
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.path.dirname(__file__), 'users.db')}"
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
 # CREATE TABLE IN DB
-
-
-class User(db.Model):
+class User(db.Model, UserMixin):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
     password: Mapped[str] = mapped_column(String(100))
@@ -38,8 +40,18 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        new_user = User(
+            email=request.form.get("email") or '',
+            password=request.form.get("password") or '',
+            name=request.form.get("name") or ''
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return redirect(url_for("secrets"))
     return render_template("register.html")
 
 
