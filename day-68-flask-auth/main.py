33410bf918237f0668a4b5318b512edcde0e12f1
matujuri <matujuri@yahoo.com>
2025-07-15 14:50:38 +0900

    SQLiteデータベースURIの設定を相対パスに変更し、アプリケーションの移植性を向上させました。また、新しいデータベースファイルを追加しました。

--- day-68-flask-auth/main.py
+++ day-68-flask-auth/main.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-
+import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
 
@@ -15,7 +15,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.path.dirname(__file__), 'users.db')}"
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
